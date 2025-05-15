22642ad0695f141c5306685b9292f21403add5cf
Yakovyakov <yyak0423@gmail.com>
2025-05-15 07:31:47 -0400

    Exercices 4a,4a,5a,5b

--- exercises/Set4a.hs
+++ exercises/Set4a.hs
@@ -34,8 +34,11 @@ import Data.Array
 -- PS. check out the error message you get with your implementation if
 -- you remove the Eq a => constraint from the type!
 
+-- remove the Eq a =>
+-- allEqual :: [a] -> Bool
 allEqual :: Eq a => [a] -> Bool
-allEqual xs = todo
+allEqual [] = True
+allEqual (x:xs) = foldr (\y acc -> y == x && acc) True xs
 
 ------------------------------------------------------------------------------
 -- Ex 2: implement the function distinct which returns True if all
@@ -50,7 +53,10 @@ allEqual xs = todo
 --   distinct [1,2] ==> True
 
 distinct :: Eq a => [a] -> Bool
-distinct = todo
+
+distinct [] = True
+
+distinct xs = if length (nub xs) == length xs then True else False
 
 ------------------------------------------------------------------------------
 -- Ex 3: implement the function middle that returns the middle value
@@ -63,7 +69,11 @@ distinct = todo
 --   middle 'b' 'a' 'c'  ==> 'b'
 --   middle 1 7 3        ==> 3
 
-middle = todo
+middle :: Ord a => a -> a -> a -> a
+middle x y z
+  | (y <= x &&  x <= z) || (z <= x && x <= y) = x
+  | (x <= y &&  y <= z) || (z <= y && y <= x) = y
+  | otherwise = z
 
 ------------------------------------------------------------------------------
 -- Ex 4: return the range of an input list, that is, the difference
@@ -78,8 +88,9 @@ middle = todo
 --   rangeOf [4,2,1,3]          ==> 3
 --   rangeOf [1.5,1.0,1.1,1.2]  ==> 0.5
 
-rangeOf :: [a] -> a
-rangeOf = todo
+rangeOf :: (Ord a, Num a) => [a] -> a
+rangeOf [] = 0
+rangeOf xs = (maximum xs) - (minimum xs)
 
 ------------------------------------------------------------------------------
 -- Ex 5: given a (non-empty) list of (non-empty) lists, return the longest
@@ -97,7 +108,12 @@ rangeOf = todo
 --   longest [[1,2,3],[4,5],[6]] ==> [1,2,3]
 --   longest ["bcd","def","ab"] ==> "bcd"
 
-longest = todo
+longest :: Ord a => [[a]] -> [a]
+longest = foldl1 (\x y ->
+  if length x > length y      then x
+  else if length x < length y then y
+  else if head x < head y     then x
+  else y) 
 
 ------------------------------------------------------------------------------
 -- Ex 6: Implement the function incrementKey, that takes a list of
@@ -113,9 +129,14 @@ longest = todo
 --   incrementKey True [(True,1),(False,3),(True,4)] ==> [(True,2),(False,3),(True,5)]
 --   incrementKey 'a' [('a',3.4)] ==> [('a',4.4)]
 
-incrementKey :: k -> [(k,v)] -> [(k,v)]
-incrementKey = todo
+incrementKey :: (Eq k, Num v) => k -> [(k,v)] -> [(k,v)]
+-- incrementKey _ [] = []
+-- incrementKey k (a:xs) = if k == fst a then (k, snd a + 1):incrementKey k xs else a:incrementKey k xs
+-- 
+-- List comprehensions
+-- incrementKey key pairs = [ if k == key then (k, v + 1) else (k, v) | (k, v) <- pairs]
 
+incrementKey key pairs = map (\(k, v) -> if k == key then (k, v + 1) else (k, v)) pairs
 ------------------------------------------------------------------------------
 -- Ex 7: compute the average of a list of values of the Fractional
 -- class.
@@ -129,7 +150,7 @@ incrementKey = todo
 -- length to a Fractional
 
 average :: Fractional a => [a] -> a
-average xs = todo
+average xs = (foldr (+) 0 xs) / fromIntegral (length xs)
 
 ------------------------------------------------------------------------------
 -- Ex 8: given a map from player name to score and two players, return
@@ -148,7 +169,9 @@ average xs = todo
 --     ==> "Lisa"
 
 winner :: Map.Map String Int -> String -> String -> String
-winner scores player1 player2 = todo
+winner scores player1 player2
+  | Map.findWithDefault 0 player1 scores >= Map.findWithDefault 0 player2 scores = player1
+  | otherwise = player2
 
 ------------------------------------------------------------------------------
 -- Ex 9: compute how many times each value in the list occurs. Return
@@ -163,8 +186,18 @@ winner scores player1 player2 = todo
 --     ==> Map.fromList [(False,3),(True,1)]
 
 freqs :: (Eq a, Ord a) => [a] -> Map.Map a Int
-freqs xs = todo
 
+-- Challenge 1: Map.alter
+-- freqs [] = Map.fromList []
+-- freqs (x:xs) = Map.alter go x (freqs xs)
+--   where
+--     go :: Maybe Int ->Maybe Int
+--     go (Just x) = Just (x + 1)
+--     go Nothing = Just 1
+
+-- Challenge 2
+
+freqs = foldr (\x acc -> Map.alter (maybe (Just 1) (Just . (+1))) x acc) Map.empty
 ------------------------------------------------------------------------------
 -- Ex 10: recall the withdraw example from the course material. Write a
 -- similar function, transfer, that transfers money from one account
@@ -191,7 +224,12 @@ freqs xs = todo
 --     ==> fromList [("Bob",100),("Mike",50)]
 
 transfer :: String -> String -> Int -> Map.Map String Int -> Map.Map String Int
-transfer from to amount bank = todo
+transfer from to amount bank
+  | from `Map.notMember` bank = bank
+  | to `Map.notMember` bank = bank
+  | amount < 0 = bank
+  | Map.findWithDefault 0 from bank < amount = bank
+  | otherwise = Map.adjust (\x -> x - amount) from (Map.adjust (\x -> x + amount) to bank)
 
 ------------------------------------------------------------------------------
 -- Ex 11: given an Array and two indices, swap the elements in the indices.
@@ -201,7 +239,7 @@ transfer from to amount bank = todo
 --         ==> array (1,4) [(1,"one"),(2,"three"),(3,"two"),(4,"four")]
 
 swap :: Ix i => i -> i -> Array i a -> Array i a
-swap i j arr = todo
+swap i j arr = arr // [(i, arr ! j), (j, arr ! i)]
 
 ------------------------------------------------------------------------------
 -- Ex 12: given an Array, find the index of the largest element. You
@@ -212,4 +250,9 @@ swap i j arr = todo
 -- Hint: check out Data.Array.indices or Data.Array.assocs
 
 maxIndex :: (Ix i, Ord a) => Array i a -> i
-maxIndex = todo
+
+-- Data.Array.indices
+-- maxIndex arr = foldr (\acc i -> if arr ! i > arr ! acc then i else acc) (head (indices arr)) (tail (indices arr))
+
+-- Data.Array.assocs
+maxIndex arr = fst $ maximumBy (comparing snd) (assocs arr)
--- exercises/Set4b.hs
+++ exercises/Set4b.hs
@@ -21,7 +21,9 @@ import Mooc.Todo
 countNothings :: [Maybe a] -> Int
 countNothings xs = foldr countHelper 0 xs
 
-countHelper = todo
+countHelper :: Maybe a -> Int -> Int
+countHelper Nothing acc = acc + 1
+countHelper (Just _) acc = acc
 
 ------------------------------------------------------------------------------
 -- Ex 2: myMaximum with a fold. Just like in the previous exercise,
@@ -35,8 +37,8 @@ myMaximum :: [Int] -> Int
 myMaximum [] = 0
 myMaximum (x:xs) = foldr maxHelper x xs
 
-maxHelper = todo
-
+maxHelper :: Int -> Int -> Int
+maxHelper x acc = max x acc
 ------------------------------------------------------------------------------
 -- Ex 3: compute the sum and length of a list with a fold. Define
 -- slHelper and slStart so that the given definition of sumAndLength
@@ -52,8 +54,10 @@ maxHelper = todo
 sumAndLength :: [Double] -> (Double,Int)
 sumAndLength xs = foldr slHelper slStart xs
 
-slStart = todo
-slHelper = todo
+slStart :: (Double, Int)
+slStart = (0.0, 0)
+slHelper :: Double -> (Double, Int) -> (Double, Int)
+slHelper x acc = (x + fst acc, snd acc + 1)
 
 ------------------------------------------------------------------------------
 -- Ex 4: implement concat with a fold. Define concatHelper and
@@ -67,8 +71,9 @@ slHelper = todo
 myConcat :: [[a]] -> [a]
 myConcat xs = foldr concatHelper concatStart xs
 
-concatStart = todo
-concatHelper = todo
+concatStart = []
+concatHelper :: [a] -> [a] -> [a]
+concatHelper x acc = x ++ acc
 
 ------------------------------------------------------------------------------
 -- Ex 5: get all occurrences of the largest number in a list with a
@@ -82,7 +87,13 @@ concatHelper = todo
 largest :: [Int] -> [Int]
 largest xs = foldr largestHelper [] xs
 
-largestHelper = todo
+largestHelper :: Int -> [Int] -> [Int]
+largestHelper x [] = [x]
+largestHelper x acc
+  | x > head acc = [x]
+  | x == head acc = [x] ++ acc
+  | otherwise = acc
+
 
 
 ------------------------------------------------------------------------------
@@ -98,7 +109,9 @@ largestHelper = todo
 myHead :: [a] -> Maybe a
 myHead xs = foldr headHelper Nothing xs
 
-headHelper = todo
+headHelper :: a -> Maybe a -> Maybe a
+headHelper x _ = (Just x)
+
 
 ------------------------------------------------------------------------------
 -- Ex 7: get the last element of a list with a fold. Define lasthelper
@@ -113,5 +126,9 @@ headHelper = todo
 myLast :: [a] -> Maybe a
 myLast xs = foldr lastHelper Nothing xs
 
-lastHelper = todo
+
+lastHelper :: a -> Maybe a -> Maybe a
+lastHelper x Nothing = (Just x)
+lastHelper x acc = acc
+
 
